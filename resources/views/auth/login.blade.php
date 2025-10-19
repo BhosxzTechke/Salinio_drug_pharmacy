c3a24e60262c79a69c21847c952a7125a092c5b7
BhosxzTechke <danmichaelantiquina9@gmail.com>
2025-10-19 15:43:25 +0800

    Fixing bootstrap js in loginform

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -121,7 +121,7 @@
 
 
         <!-- Vendor js -->
-        <script src="{{ asset('backend/assets/js/vendor.min.j') }}s"></script>
+        <script src="{{ asset('backend/assets/js/vendor.min.js') }}"></script>
 
         <!-- App js -->
         <script src="{{ asset('backend/assets/js/app.min.js') }}"></script>
