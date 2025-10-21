cb606b7114c9f395546bdae24263f7e896e64be8
BhosxzTechke <danmichaelantiquina9@gmail.com>
2025-10-21 16:10:45 +0800

    Add try catch in app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,15 +40,21 @@ class AppServiceProvider extends ServiceProvider
                 URL::forceScheme('https');
             }
 
-                // Disable ONLY_FULL_GROUP_BY in SQL mode
-            DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
+            // Disable ONLY_FULL_GROUP_BY only when DB is available
+            try {
+                DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
+            } catch (\Exception $e) {
+                info("Skipping SQL mode change: " . $e->getMessage());
+            }
 
-            // Share categories with all views
+            // Share categories safely
             View::composer('*', function ($view) {
-                $categories = cache()->rememberForever('categories', function () {
-                    return Category::all();
-                });
-                $view->with('categories', $categories);
+                try {
+                    $categories = cache()->rememberForever('categories', fn() => \App\Models\Category::all());
+                    $view->with('categories', $categories);
+                } catch (\Exception $e) {
+                    $view->with('categories', collect());
+                }
             });
 
     }
