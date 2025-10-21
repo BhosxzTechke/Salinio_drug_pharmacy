d2867ee0f2e4d3036ace2a3449a7a8ae317af609
BhosxzTechke <danmichaelantiquina9@gmail.com>
2025-10-21 15:33:14 +0800

    boot and categories in app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,11 +27,6 @@ class AppServiceProvider extends ServiceProvider
             });
     }
 
-
-    
-
-    
-
     /**
      * Bootstrap any application services.
      *
@@ -40,18 +35,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-    // Force HTTPS for all URLs in production
-    if (config('app.env') === 'production') {
-        URL::forceScheme('https');
-    }
+            // Force HTTPS for all URLs in production
+            if (config('app.env') === 'production') {
+                URL::forceScheme('https');
+            }
 
-        // Disable ONLY_FULL_GROUP_BY in SQL mode
-    DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
+                // Disable ONLY_FULL_GROUP_BY in SQL mode
+            DB::statement("SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
 
-    // Share categories with all views
-    View::composer('*', function ($view) {
-        $view->with('categories', Category::all());
-    });
+            // Share categories with all views
+            View::composer('*', function ($view) {
+                $categories = cache()->rememberForever('categories', function () {
+                    return Category::all();
+                });
+                $view->with('categories', $categories);
+            });
 
     }
 
